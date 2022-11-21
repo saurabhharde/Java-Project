c2dfd0bcc2cdb9c6369d6c0d546cbefa5efa411c
saurabhharde <118718184+saurabhharde@users.noreply.github.com>
2022-11-21 16:53:16 +0530

    Update ClockinTime.java

--- src/ClockinTime.java
+++ src/ClockinTime.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class ClockinTime {
 	public void ClockInTime(String currentId) {
+		System.out.println();
+		System.out.println("---------------- Clock In Time-----------------");
+		System.out.println();
 		Scanner scanner= new Scanner(System.in);
 		System.out.print("Enter Course Id : ");
 		System.out.println();
@@ -12,30 +15,35 @@ public class ClockinTime {
 				EnrollnewCourses.enrollCourse.get(i).getStudentid().equals(currentId)){
 				Scanner spenttime= new Scanner(System.in);
 				System.out.print("Enter time spent in Course "+C_Id+" : ");
-			try{		double timeSpent=spenttime.nextInt();
-			   		 double actualtimespent=timeSpent+EnrollnewCourses.enrollCourse.get(i).getSpenttime();
-				if (actualtimespent<=CourseDataBase.courses.get(C_Id).getDuration()) {					
+			try{	double timeSpent=spenttime.nextInt();
+					double actualtimespent=timeSpent+EnrollnewCourses.enrollCourse.get(i).getSpenttime();
+				if (actualtimespent<=CourseDataBase.courses.get(C_Id).getDuration()) {
+					
 					double completion=(actualtimespent*100)/CourseDataBase.courses.get(C_Id).getDuration();
 					EnrollnewCourses.enrollCourse.set(i, new EnrollCourse(currentId,StudentDataBase.studentData.get(currentId).getStudentName(),
 							C_Id,CourseDataBase.courses.get(C_Id).getCoursename(),
 							CourseDataBase.courses.get(C_Id).getDuration(),
 							EnrollnewCourses.enrollCourse.get(i).getEnrolldate(),actualtimespent,completion));
 					System.out.println("sucessfully add spent time in course");
-			    	     }
+				}
 				else {
-					System.out.println("Timespent more than Course duration time \nPlease enter valid time spent");
+					System.out.println();
+					System.out.println("Timespent more than Course duration time");
 					break;
-				     }
+				}
 				
-			    }
+			}
 			catch(Exception e) {
 					System.out.println("Enter time in hours");
 				}
-			}	     
+			}     
 		}	
 	}
 	
 	public void Summery(String currentId) {
+		System.out.println();
+		System.out.println("--------------------------Student Summary----------------------------");
+		System.out.println();
 		System.out.println("Student ID : "+currentId);
 		System.out.println("S tudent Name : "+StudentDataBase.studentData.get(currentId).getStudentName());
 		System.out.println("------------------------------------------------------------------------------------------------------");
@@ -53,9 +61,6 @@ public class ClockinTime {
 		}
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------------------");
-		System.out.println();
-		
-	}
-
-	
+		System.out.println();		
+	}	
 }
